48732de62ebd5eef561794a13132a1ba77d35ba6
Carlos Lantigua <clantigua954@gmail.com>
2019-02-16 22:34:55 -0500

    added devsocial and other cool stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import Nav from '../components/Nav'
 import carlos from '../assets/images/Carlos.jpg'
 import cadence from '../assets/images/cadence.gif'
 import chatify from '../assets/images/chatify.gif'
+import devsocial from '../assets/images/devsocial.gif'
 import Tech from './Tech'
 import Form from '../components/Form'
 
@@ -88,19 +89,25 @@ class Index extends React.Component {
                   </a>
                 </h3>
                 <p>
-                  Cadence is a B2B SaaS created to make shift scheduling quick
-                  and painless. Cadence was built with React, Redux, EmotionJS,
-                  React-reveal, Express, Node, PostgreSQL, Stripe, Firebase, and
-                  React-Big-Calendar.{' '}
-                  <a
-                    href="https://github.com/Lambda-School-Labs/labs9-employee-scheduler"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    view the repo
-                  </a>
+                  Employers can create organizations then add employees through
+                  our emailing service. Once registered, employees can see their
+                  weekly schedules and place their PTO Requests. Supervisors can
+                  use drag and drop to schedule as well as set their business
+                  hours for the week.
                 </p>
+                <p>
+                  ReactJS | Redux | NodeJS | ExpressJS | PostreSQL | Firebase
+                  Aauth | StripeApi | React Big Calendar
+                </p>
+                <a
+                  href="https://github.com/Lambda-School-Labs/labs9-employee-scheduler"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  view the repo
+                </a>
               </li>
+
               <li>
                 <Cadence
                   href="https://getchatify.netlify.com/"
@@ -120,33 +127,73 @@ class Index extends React.Component {
                 </h3>
                 <p>
                   Chatify was built to mimic the popular community app Slack.
-                  This site was built with a ReactJS and redux front end,
-                  followed by styled-components. The backend was built using
-                  NodeJS, ExpressJS, MongoDB, JSON-Web-Token with PassportJS and
-                  validatorJS.{' '}
+                  Users can register with passport and begin customizing their
+                  accounts to their liking. You can add a status for other
+                  members to see as well as create new channels. You can give
+                  your channel a purpose for others to follow and begin
+                  chatting.
+                </p>
+                <p>
+                  ReactJS | Redux | NodeJS | ExpressJS | MongoDB | JWT |
+                  PassportJS | ValidatorJS
+                </p>
+                <a
+                  href="https://github.com/CLantigua2/chatify"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  view the repo
+                </a>
+              </li>
+
+              <li>
+                <Cadence
+                  href="https://devsocialdotcom.herokuapp.com/"
+                  target="_blank"
+                  rel="noopner noreferrer"
+                >
+                  <img
+                    className="cadence-img"
+                    src={devsocial}
+                    alt="devsocial gif"
+                  />
+                </Cadence>
+                <h3>
+                  <a href="#" target="_blank" rel="noopener noreferrer">
+                    DevSocial - Social App for Developers
+                  </a>
+                </h3>
+                <p>
+                  DevSocial is a social media app for professionals in the
+                  development and design fields. They can use this app to
+                  communicate, share ideas and even look for potential teams
+                  based on criteria to work with.
+                </p>
+
+                <p>
+                  ReactJS | Redux | NodeJS | ExpressJS | MongoDB | JWT |
+                  PassportJS | ValidatorJS
+                </p>
+                <strong>
                   <a
-                    href="https://github.com/CLantigua2/chatify"
+                    href="https://github.com/CLantigua2/devsocial"
                     rel="noopener noreferrer"
                     target="_blank"
                   >
                     view the repo
                   </a>
-                </p>
-              </li>
-              <li>
-                <span className="icon major style5 fa-diamond" />
-                <h3>Dolor nullam</h3>
-                <p>
-                  Sed lorem amet ipsum dolor et amet nullam consequat a feugiat
-                  consequat tempus veroeros sed consequat.
-                </p>
+                </strong>
               </li>
             </ul>
             <footer className="major">
               <ul className="actions">
                 <li>
-                  <Link to="/generic" className="button">
-                    Learn More
+                  <Link
+                    to="/https://devsocialdotcom.herokuapp.com/"
+                    className="button"
+                    style={{ padding: '10px' }}
+                  >
+                    <i className="fab fa-github" style={{ fontSize: '35px' }} />
                   </Link>
                 </li>
               </ul>
